9e25a1032b8fe0cf71a911409413b7171c1451c5
Araxielfenix <araxielfenix@outlook.com>
2022-03-08 11:20:34 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 let movement = [1, 26];
+var bgsound = new Audio('sounds/Highway.mp3');
+var carEngine = new Audio('sounds/Car-engine.mp3');
+bgsound.loop();
+bgsound.volume = 0.1;
+carEngine.volume = 0.1;
+var motorsound = 0;
 window.onload = function () {
+    bgsound.play();
     document.getElementById("carLight").style.display = "none";
     document.body.style.overflow = "hidden";
+    carEngine.play();
 }
 
 document.oncontextmenu = function () {
@@ -11,7 +19,10 @@ document.oncontextmenu = function () {
 document.ondragstart = function () {
     return false;
 }
-
+function Engine() {
+    carEngine.currentTime = 3;
+    carEngine.play();
+}
 setInterval(function () {
     document.getElementById("nC").src = "./sprite/carSpeed2.png";
     setTimeout(function () {
@@ -23,15 +34,13 @@ document.onkeydown = function (e) {
     if (e.keyCode == 87 || e.keyCode == 119) {
         switch (movement[1]) {
             case 26:
+                Engine();
                 document.getElementsByClassName("car")[0].style.margin = 13 + "rem " + movement[0] + "rem";
                 movement[1] = 13;
                 break;
             case 13:
+                Engine();
                 document.getElementsByClassName("car")[0].style.margin = 2 + "rem " + movement[0] + "rem";
-                movement[1] = 2;
-                break;
-            case 2:
-                document.getElementsByClassName("car")[0].style.margin = 26 + "rem " + movement[0] + "rem";
                 movement[1] = 2;
                 break;
         }
@@ -39,46 +48,60 @@ document.onkeydown = function (e) {
     if (e.keyCode == 83 || e.keyCode == 115) {
         switch (movement[1]) {
             case 2:
+                Engine();
                 document.getElementsByClassName("car")[0].style.margin = 13 + "rem " + movement[0] + "rem";
                 movement[1] = 13;
                 break;
             case 13:
-                document.getElementsByClassName("car")[0].style.margin = 2 + "rem " + movement[0] + "rem";
+                Engine();
+                document.getElementsByClassName("car")[0].style.margin = 26 + "rem " + movement[0] + "rem";
                 movement[1] = 26;
-                break;
-            case 26:
-                document.getElementsByClassName("car")[0].style.margin = 13 + "rem " + movement[0] + "rem";
-                movement[1] = 13;
                 break;
         }
     }
     if (e.keyCode == 68 || e.keyCode == 100) {
-        if (movement[1] == 2) {
-            document.getElementsByClassName("car")[0].style.margin = 2 + "rem " + ++movement[0] + "rem";
-        }
-        if (movement[1] == 13) {
-            document.getElementsByClassName("car")[0].style.margin = 13 + "rem " + ++movement[0] + "rem";
-        }
-        if (movement[1] == 26) {
-            document.getElementsByClassName("car")[0].style.margin = 26 + "rem " + ++movement[0] + "rem";
+        switch (movement[1]) {
+            case 2:
+                Engine();
+                document.getElementsByClassName("car")[0].style.margin = 2 + "rem " + ++movement[0] + "rem";
+                movement[1] = 2;
+                break;
+            case 13:
+                Engine();
+                document.getElementsByClassName("car")[0].style.margin = 13 + "rem " + ++movement[0] + "rem";
+                movement[1] = 13;
+                break;
+            case 26:
+                Engine();
+                document.getElementsByClassName("car")[0].style.margin = 26 + "rem " + ++movement[0] + "rem";
+                movement[1] = 26;
+                break;
         }
     }
     if (e.keyCode == 65 || e.keyCode == 97) {
-        if (movement[1] == 2) {
-            document.getElementsByClassName("car")[0].style.margin = 2 + "rem " + --movement[0] + "rem";
-        }
-        if (movement[1] == 13) {
-            document.getElementsByClassName("car")[0].style.margin = 13 + "rem " + --movement[0] + "rem";
-        }
-        if (movement[1] == 26) {
-            document.getElementsByClassName("car")[0].style.margin = 26 + "rem " + --movement[0] + "rem";
+        switch (movement[1]) {
+            case 2:
+                Engine();
+                document.getElementsByClassName("car")[0].style.margin = 2 + "rem " + --movement[0] + "rem";
+                movement[1] = 2;
+                break;
+            case 13:
+                Engine();
+                document.getElementsByClassName("car")[0].style.margin = 13 + "rem " + --movement[0] + "rem";
+                movement[1] = 13;
+                break;
+            case 26:
+                Engine();
+                document.getElementsByClassName("car")[0].style.margin = 26 + "rem " + --movement[0] + "rem";
+                movement[1] = 26;
+                break;
         }
     }
     if (e.keyCode == 69 || e.keyCode == 101) {
         document.getElementById("carLight").style.display = "block";
+        document.getElementById("honk").volume = 0.1;
         document.getElementById("honk").pause();
         document.getElementById("honk").currentTime = 0;
-        document.getElementById("honk").volume = 0.2;
         document.getElementById("honk").play();
     }
 }
