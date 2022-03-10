c0f0ca1733d19a67b07d5fb55c97b7bd93959c50
Araxielfenix <araxielfenix@outlook.com>
2022-03-10 09:28:31 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -56,6 +56,7 @@ function Honk() {
 }
 
 function dpadUp() {
+    carOutOfScreen();
     if (motorsound == 0) {
         bgsound.play();
         carEngine.play();
@@ -73,6 +74,7 @@ function dpadUp() {
     }
 }
 function dpadDown() {
+    carOutOfScreen();
     if (motorsound == 0) {
         bgsound.play();
         carEngine.play();
@@ -90,10 +92,12 @@ function dpadDown() {
     }
 }
 function dpadRight() {
+    carOutOfScreen();
     if (motorsound == 0) {
         bgsound.play();
         carEngine.play();
         motorsound++;
+        enemySpawn();
     }
     switch (movement[1]) {
         case 2:
@@ -108,6 +112,7 @@ function dpadRight() {
     }
 }
 function dpadLeft() {
+    carOutOfScreen();
     if (motorsound == 0) {
         bgsound.play();
         carEngine.play();
@@ -136,6 +141,8 @@ document.onkeydown = function (e) {
         bgsound.play();
         carEngine.play();
         motorsound++;
+        enemySpawn();
+        carOutOfScreen();
     }
     if (e.keyCode == 87 || e.keyCode == 119) {
         switch (movement[1]) {
@@ -196,5 +203,58 @@ document.onkeyup = function (e) {
     if (e.keyCode == 69 || e.keyCode == 101) {
         document.getElementById("carLight").style.margin = "-12.78rem 5.5rem";
         document.getElementById("carLight").style.display = "none";
+    }
+}
+function enemySpawn(){
+    // Variable random del 1 al 3 para que el enemigo salga de una de las 3 posiciones.
+    var random = Math.floor(Math.random() * 3) + 1;
+    // Si el random es 1, el enemigo aparecerá en la posición 1.
+    if (random == 1) {
+        document.getElementsByClassName("enemy")[0].style.display = "block";
+        document.getElementsByClassName("enemy")[0].style.margin = "-60rem 37rem;";
+    }
+    // Si el random es 2, el enemigo aparecerá en la posición 2.
+    if (random == 2) {
+        document.getElementsByClassName("enemy")[0].style.display = "block";
+        document.getElementsByClassName("enemy")[0].style.margin = "-73rem 37rem;";
+    }
+    // Si el random es 3, el enemigo aparecerá en la posición 3.
+    if (random == 3) {
+        document.getElementsByClassName("enemy")[0].style.display = "block";
+        document.getElementsByClassName("enemy")[0].style.margin = "-85rem 37rem;";
+    }
+}
+// Si la imagen enemy está visible y la imagen carLight está visible.
+function enemyCollision() {
+    if (document.getElementsByClassName("enemy")[0].style.display == "block" && document.getElementById("carLight").style.display == "block") {
+        // Si la posición de la imagen carLight es igual a la posición de la imagen enemy.
+        if (document.getElementById("carLight").style.margin == document.getElementsByClassName("enemy")[0].style.margin) {
+            // Se le quita la vida al jugador.
+            life--;
+            // Se oculta la imagen carLight.
+            document.getElementById("carLight").style.display = "none";
+            // Se oculta la imagen enemy.
+            document.getElementsByClassName("enemy")[0].style.display = "none";
+            // Se muestra la imagen explosion.
+            document.getElementsByClassName("explosion")[0].style.display = "block";
+            // Se reproduce el sonido de explosion.
+            explosion.play();
+            // Se oculta la imagen explosion.
+            setTimeout(function () {
+                document.getElementsByClassName("explosion")[0].style.display = "none";
+            }, 1000);
+        }
+    }
+}
+// prevenir que el carro se salga de la pantalla.
+function carOutOfScreen() {
+    if (movement[0] < -1) {
+        movement[0] = -1;
+        document.getElementsByClassName("car")[0].style.margin = movement[1] + "rem " + movement[0] + "rem";
+
+    }
+    if (movement[0] > 65) {
+        movement[0] = 65;
+        document.getElementsByClassName("car")[0].style.margin = movement[1] + "rem " + movement[0] + "rem";
     }
 }
